01c95ea3856aee0ea0dd9c77aef515ca2d3b590c
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-09-16 23:24:06 +0530

    1 file modified

--- index.js
+++ index.js
@@ -7,9 +7,33 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 function FETCH_CHATBOX(post_url, text) {
   console.log(post_url, text);
+  fetch(post_url, {
+    method: 'POST',
+    body: JSON.stringify({
+      message: text,
+    }),
+    mode: 'cors',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((r) => r.json())
+    .then((r) => {
+      let msg2 = {
+        name: 'AI',
+        message: r.answer,
+      };
+      console.log(msg2);
+      //this.messages.push(msg2);
+      //this.updateChatText(chatbox);
+      //textField.value = '';
+    })
+    .catch((error) => {
+      console.error(error);
+      //this.updateChatText(chatbox);
+      //textField.value = '';
+    });
 }
 
-FETCH_CHATBOX(
-  'https://impactchatbotapi.newgen.co:8002/predict',
-  'how to insert new reference'
-);
+FETCH_CHATBOX('', 'how to insert new reference');
