ba930a313fea6f28d6cd4a77138b65f75085ad13
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-09-16 23:22:10 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function FETCH_CHATBOX(post_url, text) {
+  console.log(post_url, text);
+}
+
+FETCH_CHATBOX(
+  'https://impactchatbotapi.newgen.co:8002/predict',
+  'how to insert new reference'
+);
